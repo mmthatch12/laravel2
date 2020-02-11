90be30a80d8d8153eb0202a3ae82475a4109d783
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2020-02-11 14:26:04 -0700

    added auth, first migration, and some styling to app.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Second Laravel Project
                 </div>
 
                 <div class="links">
