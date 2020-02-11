347a6da2640cbd422200a3f1ef493521e51317ba
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2020-02-11 15:51:12 -0700

    added more to home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,32 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <div class="row">
+        <div class="col-3">
+            <img src="/img/space-walk.png" class="rounded-circle" style="width: 150px;" alt="">
+        </div>
+        <div class="col-9">
+            <div><h1>Second Laravel Project</h1></div>
+            <div class='d-flex'>
+                <div class="pr-5"><strong>153</strong> posts</div>
+                <div class="pr-5"><strong>23k</strong> followers</div>
+                <div class="pr-5"><strong>212</strong> following</div>
             </div>
+            <div class='pt-4 font-weight-bold'>Second Laravel Project</div>
+            <div>This is the second laravel project I've worked on</div>
+            <div> <a href="#">www.freecodecamp.org</a></div>
+        </div>
+    </div>
+
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="/img/space-walk.png" class="w-100" alt="">
+        </div>
+        <div class="col-4">
+            <img src="/img/space-walk.png" class="w-100" alt="">
+        </div>
+        <div class="col-4">
+            <img src="/img/space-walk.png" class="w-100" alt="">
         </div>
     </div>
 </div>
